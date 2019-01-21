b0b74792b009f6a804c2dae290fab1e46509478f
zjhellofss <46443564+zjhellofss@users.noreply.github.com>
2019-01-21 18:13:26 +0800

    提供pdf转换的功能

---
+++ markdown_src/markdown_convert/markdown_html2pdf.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void convert_html2pdf (const char *filename) {
+    char *command = malloc(512);
+    char *filename_prefix_h = malloc(sizeof(char) * 128);
+    memset(filename_prefix_h, '\0', 128);
+    char *file_ = strchr(filename, '.');
+    strncpy(filename_prefix_h, filename, file_ - filename);
+    strcpy(filename_prefix_h + strlen(filename_prefix_h), ".html");
+
+    char *filename_prefix = malloc(sizeof(char) * 128);
+    memset(filename_prefix, '\0', 128);
+    strncpy(filename_prefix, filename, file_ - filename);
+    strcpy(filename_prefix + strlen(filename_prefix), ".pdf");
+
+    sprintf(command, "%s %s %s %s", "wkhtmltopdf", "--footer-font-name 'Menlo' --footer-right [page] --encoding utf-8 --no-stop-slow-scripts ", filename_prefix_h, filename_prefix);
+    system(command);
+    free(filename_prefix);
+    free(filename_prefix_h);
+    free(command);
+}
---
+++ markdown_src/markdown_convert/markdown_html2pdf.h
@@ -0,0 +1,7 @@
+
+#ifndef PARSER_MARKDOWN_HTML2PDF_H
+#define PARSER_MARKDOWN_HTML2PDF_H
+
+void convert_html2pdf (const char *filename);
+
+#endif //PARSER_MARKDOWN_HTML2PDF_H
